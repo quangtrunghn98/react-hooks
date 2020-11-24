05716296294255c70080d893772da804c963335d
Ngo Quang Trung <quangtrunghn98@gmail.com>
2020-11-24 11:09:18 +0700

    add feature Todoform to add item

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import { useState } from 'react';
 import './App.css';
+import TodoForm from './components/TodoForm';
 import TodoList from './components/TodoList';
 
 function App() {
@@ -18,10 +19,21 @@ function App() {
     setTodoList(newTodoList);
   }
 
+  function handleTodoFormSubmit(formValue) {
+    const newTodo = {
+      id: TodoList.length + 1,
+      ...formValue,
+    }
+    const newTodoList = [...todoList];
+    newTodoList.push(newTodo);
+    setTodoList(newTodoList);
+  }
+
   return (
     <div className="App">
       <h1>React hook - Todolist</h1>
 
+      <TodoForm onSubmit={handleTodoFormSubmit} />
       <TodoList todos={todoList} onTodoClick={handleTodoClick} />
     </div>
   );
