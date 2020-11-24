553f93b7c759f56c414cf0a6cf9c6aa30c5c56cb
Ngo Quang Trung <quangtrunghn98@gmail.com>
2020-11-24 09:48:13 +0700

    add TodoList

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
-import ColorBox from './components/ColorBox';
+import TodoList from './components/TodoList';
 
 function App() {
+  const [todoList, setTodoList] = useState([
+    { id: 1, title: 'i love fontend'},
+    { id: 1, title: 'we love fontend'},
+    { id: 1, title: 'they love fontend'},
+  ]);
+
+  function handleTodoClick(todo) {
+    const index = todoList.findIndex(x => x.id === todo.id);
+    if (todo < 0) return;
+    const newTodoList = [...todoList];
+    newTodoList.splice(index, 1);
+    setTodoList(newTodoList);
+  }
+
   return (
     <div className="App">
-      <h1>Welcome</h1>
+      <h1>React hook - Todolist</h1>
 
-      <ColorBox/>
+      <TodoList todos={todoList} onTodoClick={handleTodoClick} />
     </div>
   );
 }
